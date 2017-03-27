410ce2a958d6dc1633421ad00a4c75930a1a873d
Konstantin Mamaev <konstantin.mamaev@akvelon.com>
2017-03-27 18:49:05 +0300

    Revert Globalize hack

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,32 @@
+interface GlobalizeCulture {
+    name: string;
+    calendar: GlobalizeCalendar;
+    calendars: CalendarDictionary;
+    numberFormat: GlobalizeNumberFormat;
+}
+interface GlobalizeCalendar {
+    patterns: any;
+    firstDay: number;
+}
+interface CalendarDictionary {
+    [key: string]: GlobalizeCalendar;
+}
+interface GlobalizeNumberFormat {
+    decimals: number;
+    groupSizes: number[];
+    negativeInfinity: string;
+    positiveInfinity: string;
+}
+interface GlobalizeStatic {
+    culture(cultureSelector?: string): GlobalizeCulture;
+    format(value: any, format: string, culture?: any): any;
+    parseInt(value: string, culture?: any): number;
+    parseFloat(value: string, culture?: any): number;
+    parseDate(value: string, formats: string[], culture?: any): Date;
+    cultures: any;
+    findClosestCulture(cultureSelector: string): GlobalizeCulture;
+}
+declare var Globalize: GlobalizeStatic;
 declare module powerbi.extensibility.utils.formatting {
     interface IStorageService {
         getData(key: string): any;
